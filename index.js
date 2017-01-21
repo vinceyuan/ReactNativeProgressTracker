9e236d14618e10a27a0eff62bc0828bd847e23cb
Vince Yuan <vince.yuan@gmail.com>
2017-01-21 23:29:40 +0800

    Implement progress tracker. Use 2 triangles to build an arrow.

--- index.js
+++ index.js
@@ -7,18 +7,96 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Dimensions,
+  Platform,
   StyleSheet,
   Text,
-  View
+  TouchableOpacity,
+  View,
 } from 'react-native';
 
+const WIDTH = Dimensions.get('window').width;
+const HEIGHT = 56;
+const ARROW_WIDTH = 16;
+
+
 export default class ReactNativeProgressTracker extends Component {
+
+  // _arrowView() returns a view containing two triangles, A and B when index is 0, or C and D when index is 1.
+  // B is a little bigger than A. A is above B in different color to form the shape of >.
+  //
+  // --------
+  // |\\  C |
+  // | \\   |
+  // |  \\  |
+  // |   \\ |
+  // |A/B \\|
+  // |   // |
+  // |  //  |
+  // | //   |
+  // |//  D |
+  // --------
+  _arrowView(index: number) {
+    return ( index == 0 ?
+      <View style={[styles.leftArrowView]}>
+        <View style={styles.triangleB}></View>
+        <View style={styles.triangleA}></View>
+      </View>
+      :
+      <View style={[styles.rightArrowView]}>
+        <View style={styles.triangleC}></View>
+        <View style={styles.triangleD}></View>
+      </View>
+    );
+  }
+
+  _progressTracker() {
+    let items = ['Button 0', 'Button 1'];
+    let shouldShowArrow = items.length == 2;
+
+    return items.map((item, index) => {
+      let left = 0, right = 0;
+      let marginLeft = 0, marginRight = 0;
+
+      if (shouldShowArrow) {
+        if (index == 1) { // The right one
+          left = (WIDTH - ARROW_WIDTH) / 2; right = 0;
+          marginLeft = ARROW_WIDTH; marginRight = 0;
+        } else {
+          left = 0; right = (WIDTH - ARROW_WIDTH) / 2;
+          marginLeft = 0; marginRight = ARROW_WIDTH;
+        }
+      }
+
+      let positionStyle = {
+        position: 'absolute',
+        top: 0, left, right,
+        height: HEIGHT,
+      };
+
+      return (
+        <TouchableOpacity key={index}
+          style={positionStyle}>
+          <View style={[styles.flex, styles.center, styles.bgColor, {marginLeft, marginRight}]}>
+            <Text style={styles.titleActionText}>{item}</Text>
+          </View>
+          {shouldShowArrow?this._arrowView(index):null}
+        </TouchableOpacity>
+      );
+
+    });
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           ReactNativeProgressTracker
         </Text>
+        <View style={styles.progressTracker}>
+          { this._progressTracker() }
+        </View>
       </View>
     );
   }
@@ -35,6 +113,95 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginTop: 20,
   },
+  progressTracker: {
+    height: HEIGHT,
+    width: WIDTH,
+    overflow: 'hidden',
+  },
+  flex: {
+    flex: 1,
+  },
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bgColor: {
+    backgroundColor: '#252831',
+  },
+  titleActionText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'green',
+  },
+
+  //Arrow
+  leftArrowView: {
+    position: 'absolute',
+    top: 0,
+    height: HEIGHT,
+    width: ARROW_WIDTH,
+    right:0,
+  },
+  rightArrowView: {
+    position: 'absolute',
+    top: 0,
+    height: HEIGHT,
+    width: ARROW_WIDTH,
+    left:0
+  },
+  triangleA: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: 0,
+    height: 0,
+    borderTopWidth: HEIGHT / 2,
+    borderBottomWidth: HEIGHT / 2 + 2,
+    borderLeftWidth: ARROW_WIDTH - 3,
+    borderTopColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderLeftColor: '#252831',
+  },
+  triangleB: {
+    position: 'absolute',
+    top: -6,
+    left: 0,
+    width: 0,
+    height: 0,
+    borderTopWidth: HEIGHT / 2 + 6,
+    borderBottomWidth: HEIGHT / 2 + 8,
+    borderLeftWidth: ARROW_WIDTH,
+    borderTopColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderLeftColor: 'gray',
+  },
+  triangleC: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: 0,
+    height: 0,
+    borderLeftWidth: ARROW_WIDTH - 2.5,
+    borderRightWidth: 0,
+    borderTopWidth: HEIGHT / 2 + (Platform.OS === 'ios'?1:0),
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderTopColor: '#252831',
+  },
+  triangleD: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    width: 0,
+    height: 0,
+    borderLeftWidth: ARROW_WIDTH - 3,
+    borderRightWidth: 0,
+    borderBottomWidth: HEIGHT / 2 + 1,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: '#252831',
+  },
+
 });
 
 AppRegistry.registerComponent('ReactNativeProgressTracker', () => ReactNativeProgressTracker);
